aa03dfe9cc6aeca2399f7b6c22ef6f4cdd8a760a
Bruno S Marques <brunosmarques1995@hotmail.com>
2024-07-03 01:11:17 +0000

    Added extra configs

--- src/SensorMock.hpp
+++ src/SensorMock.hpp
@@ -2,12 +2,14 @@
 #include <chrono>
 #include <string>
 #include <fstream>
+#include <mutex>
+#include <memory>
 
 namespace SensorProject
 {
     struct LogRecord {
         const std::string sensor_id; // supondo um ID de sensor de até 32 caracteres
-        std::time_t timestamp; // timestamp UNIX
+        std::chrono::steady_clock::time_point timestamp; // timestamp UNIX
         double value; // valor da leitura
         LogRecord(std::string_view id):
             sensor_id(id)
@@ -19,14 +21,17 @@ namespace SensorProject
     class SensorMock
     {
     public:
-        SensorMock(std::string_view sensorID, size_t sensorPause, double sensorRange, double sensorOffset);
+        SensorMock(std::string_view sensorID, size_t sensorPause, double sensorRange, double sensorOffset, std::shared_ptr<std::fstream> logFile, std::shared_ptr<std::mutex> sharedMutex);
         ~SensorMock() = default;
 
         void SimSensor();
     private:
+
         LogRecord m_CurrentLog;
-        size_t m_SensorPause; 
-        double m_SensorRange;
+        size_t m_SensorPause;
+        std::shared_ptr<std::fstream> m_Logger;
+        std::shared_ptr<std::mutex> m_SharedMutex;
+        double m_SensorAmplitude;
         double m_SensorOffset;
         void GenRandomLog();
         void RecordLog();
